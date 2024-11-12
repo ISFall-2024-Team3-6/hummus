e01257683585dfee1ab8fc5ca3288a31282c34a4
porterwg <porterwg@byu.edu>
2024-11-12 16:37:01 -0700

    ejs

---
+++ index.js
@@ -0,0 +1,37 @@
+let express = require("express");
+
+let app = express();
+
+let path = require("path");
+
+const port = 5001;
+
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({extended: true}));
+
+const knex = require("knex") ({
+    client : "pg",
+    connection : {
+        host : "localhost",
+        user : "postgres",
+        password : "ISGoesCrazyy",
+        database : "assignment3a",
+        port : 5432
+    }
+})
+
+app.get("/", (req, res) =>
+{
+    knex.select().from('pokemon').orderBy('description').then( pokes => {
+        res.render("index", { pokemon: pokes });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({err});
+    });
+});
+
+
+app.listen(port, () => console.log("Express App has started and server is listening!"));
